de4294bd6fc6870d3f275b643e3155a8c006c1a0
BykAndrey <andreybyk9606@gmail.com>
2020-01-12 15:08:03 +0300

    init

---
+++ src/components/Interface/Leftmenu/ControllMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class ControllMenu extends React.Component {
+    render() {
+        return (<div>
+            <h3>Menu</h3>
+        </div>)
+    }
+}
+export default ControllMenu;
---
+++ src/components/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ControllMenu from './Interface/Leftmenu/ControllMenu';
+
+class Root extends React.Component{
+    render() {
+        return (<div>
+            <h1>3
+
+            </h1>
+            <ControllMenu/>
+        </div>);
+    }
+}
+export default Root;
